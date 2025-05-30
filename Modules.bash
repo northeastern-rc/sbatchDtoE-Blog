ebca0d916ad20e6350f9f640dfe0d459d5a02f35
Tara Ketchem <101415920+tketchem14@users.noreply.github.com>
2025-05-30 11:25:16 -0400

    Create Modules.bash

---
+++ Modules.bash
@@ -0,0 +1,20 @@
+[s.caplins@explorer-01 sbatch]$ module avail
+------------------------------------------------------ /shared/EL9/explorer/modulefiles ------------------------------------------------------
+admin/2018-10-10   code-server/4.91.1       go/1.23.2                    julia/1.10.4        nvidia-hpc-sdk/24.7  schrodinger/2024-4
+AMBER/24           cuda/12.1.1              gromacs/2024.3               knime/5.3.1         OpenBLAS/0.3.29      sratoolkit/12Dec2024
+anaconda3/2024.06  cuda/12.3.0              gurobi/11.0.3                LAMMPS/29Aug2024    OpenCV/4.10.0        star/2.7.11b
+bamtools/2.5.2     cuda/12.3.0.old          htop/3.3.0                   mathematica/14.1.0  OpenFOAM/12          Tinker/8.11.3
+bbmap/39.11        cuda/12.8.0              igv/2.18.4                   matlab/R2023b       OpenJDK/22.0.2       trimmomatic/0.39
+bcftools/1.21      discovery/1.0            intel/compilers-2025.0.4     matlab/R2024b       OpenMPI/4.1.6        vcftools/0.1.16
+bedtools/2.31.1    EMAN2/2.99.47            intel/compilers-rt-2025.0.4  miniconda3/24.11.1  ParaView/5.13.0-RC2  VMD/1.9.3
+blast/2.13.0       explorer/1.0             intel/mkl-2025.0             MPICH/4.3.0b1       ParaView/5.13.1      VMD/1.9.4a55
+bowtie2/2.5.4      fastqc/0.12.1            intel/mpi-2021.14            netlogo/6.4.0       perl/5.40.0          VTune/2025.0
+bwa/0.7.18         gaussian/g16             intel/tbb-2022.0             nextflow/24.10.3    R/4.4.1              weka/3.8.6
+clustal/2.1        glew/2.2.0               intel/umf-0.9.1              nodejs/v22.11.0     ROCm/6.3.0
+cmake/3.30.2       GNU-parallel/2025-04-22  job-assist/1.0               Nsight/2024.7.1     samtools/1.21
+
+------------------------------------------------------- /usr/share/Modules/modulefiles -------------------------------------------------------
+dot  module-git  module-info  modules  null  use.own
+
+Key:
+loaded  modulepath
