8a5f3917cde287945662db0afc30c4b2c3499c67
Tara Ketchem <101415920+tketchem14@users.noreply.github.com>
2025-05-30 11:12:10 -0400

    Create ExplorerScript.bash

---
+++ ExplorerScript.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p short
+#SBATCH -t 5:00:00
+#SBATCH -n 32
+#SBATCH --mem=15G
+
+# Define container image path
+SINGULARITY_IMAGE="/projects/seedpod/container/seedpod_rstudio.sif"
+
+# Define input data paths
+BAMLIST="/projects/seedpod/rawdata/bam_lists/spal/bam_list"
+REF="/projects/seedpod/reference/updated.fasta"
+OUTDIR="/projects/seedpod/output/angsd_spal_SC"
+ANGSD_OPTIONS="-GL 2 -doMajorMinor 1  -doGlf 2 -doMaf 2 -SNP_pval 1e-6 -minMapQ 30 -minQ 20 -minMaf 0.05 -minInd 300"
+
+# Default to 32 CPUs if SLURM_CPUS_PER_TASK is not set
+SLURM_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK:-32}
+
+# Run container image with singularity
+
+apptainer exec -B "/projects:/projects" $SINGULARITY_IMAGE \
+         angsd -b $BAMLIST -ref $REF -out $OUTDIR/redo${site}_list $ANGSD_OPTIONS -P ${SLURM_CPUS_PER_TASK}
