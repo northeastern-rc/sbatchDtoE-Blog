557601e25adfffdf4821d24ad1d61d3e36ce6701
Tara Ketchem <101415920+tketchem14@users.noreply.github.com>
2025-05-30 11:21:53 -0400

    Create ContainerImages.bash

---
+++ ContainerImages.bash
@@ -0,0 +1,2 @@
+[s.caplins@explorer-01 sbatch]$ ls /shared/container_repository/explorer/
+afni  angsd  courses  fsl  gatk  megan_diamond  multiqc  phpmyadmin  rstudio  salmon  star  tensorboard  trinityrnaseq  trinotateweb  yasara
